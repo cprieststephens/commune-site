6c1bb99ad9bc025d37943b104ce9c7aab4c17c38
Camilla Priest-Stephens <cprieststephens@googlemail.com>
2022-11-16 14:09:09 +0000

    add interactive hamburger menu for mobile screen

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+let navMenu = document.getElementById("dropdown");
+let navToggle = document.getElementById("hamburger");
 
+navToggle.addEventListener("click", function () {
+  navMenu.classList.toggle("active");
+});
