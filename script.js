39faa5bb4c1ddda7993bea48f224b066236fb5fa
Camilla Priest-Stephens <cprieststephens@googlemail.com>
2022-11-22 06:31:47 +0000

    adjust position of dropdown menu and validation message on tablet screens

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // To show dropdown menu when hamburger menu is clicked
 let navMenu = document.getElementById("dropdown");
 let navToggle = document.getElementById("hamburger");
-
 navToggle.addEventListener("click", function () {
   navMenu.classList.toggle("active");
 });
